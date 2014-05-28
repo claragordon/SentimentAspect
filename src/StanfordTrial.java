dae5df771d00b13843ee667b45ca7dbb4aedaa85
Jared Kramer <jaredkanekramer@gmail.com>
2014-05-28 12:43:07 -0700

    folioing around with stanford

--- src/StanfordTrial.java
+++ src/StanfordTrial.java
@@ -9,41 +9,22 @@ import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
 import edu.stanford.nlp.trees.*;
 import edu.stanford.nlp.util.*;
 
-public class StanfordTrial {
+
+public class StanfordTrial{
 
    public static void main(String[] args) throws IOException {
-       // creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution 
-       Properties props = new Properties();
-       props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse");
-       StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-       
-       // read some text in the text variable
-       String text = "This is my text sentence."; // Add your text here!
-       
-       // create an empty Annotation just with the given text
-       Annotation document = new Annotation(text);
-       
-       // run all Annotators on this text
-       pipeline.annotate(document);    
-       
-       // these are all the sentences in this document
-       // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
-       List<CoreMap> sentences = document.get(SentencesAnnotation.class);
-       
-//        for(CoreMap sentence: sentences) {
-//          // traversing the words in the current sentence
-//          // a CoreLabel is a CoreMap with additional token-specific methods
-//           for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
-//               // this is the text of the token
-//               String word = token.get(TextAnnotation.class);
-//               // this is the POS tag of the token
-//               String pos = token.get(PartOfSpeechAnnotation.class);
-//               // this is the NER label of the token
-//               String ne = token.get(NamedEntityTagAnnotation.class);       
-//             }
-//        }
- 
- 
- 
+   String xmlFile = "../data/train/laptop--train.xml";
+   List<Sentence> sentences = XMLReader.readFile(xmlFile);
+   
+   Sentence s = sentences.get(0);
+   List<Aspect> list_of_a = s.getAspects();
+   Aspect a = list_of_a.get(0);
+
+   StanfordPipeLine p = new StanfordPipeLine();
+   String tagged = p.posString(s.getText(), a);
+   System.out.println(tagged);
+   
+
    }
+      
 }
